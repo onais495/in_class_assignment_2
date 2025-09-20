4195c792e21b8f2b2deb94a2b0c17e49ec0696b2
onais495 <muhammadonais495@gmail.com>
2025-09-20 01:25:59 -0400

    finished

--- app.js
+++ app.js
@@ -30,24 +30,18 @@ let myserver = http.createServer(function (req, res) {
 		{
 			schedule(query, res);
 		}
-		else
-			sendResponse(404, "Day, time and name required.", res);
 		break;
 		case "/cancel":
 		if (validateQuery(query, true))
 		{
 			cancel(query, res);
 		}
-		else
-			sendResponse(404, "Day, time and name required.", res);
 		break;
 		case "/check":
 		if (validateQuery(query, false))
 		{
 			check(query, res);
 		}
-		else
-			sendResponse(404, "Day and time required.", res);
 		break;
 		default:
 			sendResponse(404, "pathname unknown", res);
@@ -61,18 +55,6 @@ function sendResponse(status, message, res)
 	res.writeHead(status, {'Content-Type': 'text/plain'});
 	res.write(message);
 	res.end();
-}
-
-//Check if the day, time or name are missing from the query
-//nameRequired checks if validateqQuery is being called from schedule, cancel or check
-function validateQuery(query, nameRequired)
-{
-	const required = ["day", "time"];
-	if (nameRequired)
-	{
-		required.push("name");
-	}
-	return required.every(field => query[field] != undefined);
 }
 
 //check if the first character of the query day is uppercase, if not capitalize it and make the rest lowercase
@@ -127,48 +109,84 @@ function checkName(query, res)
 	}
 }
 
+//Check if the day, time or name are missing from the query
+//nameRequired checks if validateqQuery is being called from schedule, cancel or check
+//if query is valid, check if the day, time or name are in correct format
+function validateQuery(query, nameRequired, res)
+{
+	const required = ["day", "time"];
+	if (nameRequired)
+	{
+		required.push("name");
+	}
+	let isQueryValid = required.every(field => query[field] != undefined);
+	if (!isQueryValid)
+	{
+		if (nameRequired)
+		{
+			sendResponse(404, "Missing day, time or name", res);
+		}
+		else if (!nameRequired)
+		{
+			sendResponse(404, "Missing day or time", res);
+		}
+		return false;
+	}
+	if (isQueryValid && nameRequired)
+	{
+		if (checkName(query, res) && checkDay(query, res) && checkTime(query, res))
+		{
+			return true;
+		}
+		else
+		return false;
+	}
+	else if (isQueryValid && !nameRequired)
+	{
+		if (checkDay(query, res) && checkTime(query, res))
+		{
+			return true;
+		}
+		else
+		return false;
+	}
+}
+
 //Check if an appointment is available
 function check(query, res)
 {
 	//first check if the day exists in availablTimes to avoid undefined errors
 	//then check fi the given time exists in the array for that day
-	if (checkDay(query, res) && checkTime(query, res))
+	
+	if (availableTimes[query.day].some(time => time == query.time))
 	{
-		if (availableTimes[query.day].some(time => time == query.time))
-		{
-			sendResponse(200, "Available", res);
-			return;
-		}
-		else
-		sendResponse(404, "Not Available", res);
+		sendResponse(200, "Available", res);
+		return;
 	}
-}	
-
+	else
+	sendResponse(404, "Not Available", res);
+}
 
 function schedule(query, res)
 {
-	//check if the given day exists, check if the time is in correct format
-	//then check if a certain time exists in availableTimes on a specific day
+	//Check if a certain time exists in availableTimes on a specific day
 	//if it does, remove if from availbleTimes and add an appointment using 
 	//the name, day and time from the query
-	if (checkDay(query, res) && checkName(query,res) && checkTime(query, res))
+	if (availableTimes[query.day].some(time => time == query.time))
 	{
-		if (availableTimes[query.day].some(time => time == query.time))
+		for (let i = 0; i < availableTimes[query.day].length; i++)
 		{
-			for (let i = 0; i < availableTimes[query.day].length; i++)
+		if (availableTimes[query.day][i] == query.time)
 			{
-			if (availableTimes[query.day][i] == query.time)
-				{
-					availableTimes[query.day].splice(i, 1);
-					appointments.push({name: query.name, day: query.day, time: query.time});
-					sendResponse(200, "Reserved", res);
-					return;
-				}
+				availableTimes[query.day].splice(i, 1);
+				appointments.push({name: query.name, day: query.day, time: query.time});
+				sendResponse(200, "Reserved", res);
+				return;
 			}
 		}
-		else
-		sendResponse(404, "Could not schedule", res);
 	}
+	else
+	sendResponse(404, "Could not schedule", res);
 }
 
 //cancel the appointment if it exists
@@ -176,27 +194,23 @@ function cancel(query, res)
 {
 	//check if the appointment exists, 
 	//if it does, remove if from appointments and add the time back to availableTImes
-	if (checkDay(query, res) && checkName(query,res) && checkTime(query, res))
+	if (appointments.some(element => element.name == query.name && element.day == query.day && element.time == query.time))	
 	{
-		if (appointments.some(element => element.name == query.name && element.day == query.day && element.time == query.time))	
+		for (let i = 0; i < appointments.length; i++)
 		{
-			for (let i = 0; i < appointments.length; i++)
+			if (appointments[i].name == query.name && appointments[i].day == query.day && appointments[i].time == query.time)
 			{
-				if (appointments[i].name == query.name && appointments[i].day == query.day && appointments[i].time == query.time)
-				{
-					appointments.splice(i, 1);
-					availableTimes[query.day].push(query.time);
-					console.log(appointments);
-					console.log(availableTimes);
-					sendResponse(200, "Appointment has been cancelled", res);
-					return;
-				}
+				appointments.splice(i, 1);
+				availableTimes[query.day].push(query.time);
+				console.log(appointments);
+				console.log(availableTimes);
+				sendResponse(200, "Appointment has been cancelled", res);
+				return;
 			}
 		}
-		else
-		sendResponse(404, "Appointment not found", res);
 	}
+	else
+	sendResponse(404, "Appointment not found", res);
 }
-
 
 myserver.listen(80); //the server object listens on port 80
