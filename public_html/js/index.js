1a85cf040cff776fdb7436c87c6aa1ede96288af
onais495 <muhammadonais495@gmail.com>
2025-10-12 04:22:23 -0400

    Added event handlers for the 3 buttons, corrected the html file by adding button ids rather than types

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,1 +1,84 @@
-alert("Testing File Server");
+document.getElementById("schedule").addEventListener("click", scheduleAppointment);
+
+document.getElementById("cancel").addEventListener("click", cancelAppointment);
+
+document.getElementById("check").addEventListener("click", checkAvailability);
+
+let inputName = document.getElementById("name");
+
+let inputDay = document.getElementById("day");
+
+let inputTime = document.getElementById("Time");
+
+let output = document.getElementById("results");
+
+function errorHandler(message)
+{
+    alert("Error: " + message);
+}
+
+function scheduleAppointment()
+{
+    let AJAXObj = new XMLHttpRequest();
+    AJAXObj.onload = function()
+    {
+        if (this.status == 200)
+        {
+            output.innerHTML = this.responseText;   
+        }
+        else
+        {
+            errorHandler("Status: " + this.status + this.responseText);
+        }
+    };
+    AJAXObj.onerror = function()
+    {
+        errorHandler("Connection error. Try again later.");
+    }
+    AJAXObj.open("GET", "/schedule?name=" + inputName.value + "&day=" + inputDay.value + "&time=" + inputTime.value);
+    AJAXObj.send();
+}
+
+function cancelAppointment()
+{
+    let AJAXObj = new XMLHttpRequest();
+    AJAXObj.onload = function()
+    {
+        if (this.status == 200)
+        {
+            output.innerHTML = this.responseText;   
+        }
+        else
+        {
+            errorHandler("Status: " + this.status + this.responseText);
+        }
+        };
+    AJAXObj.onerror = function()
+    {
+        errorHandler("Connection error. Try again later.");
+    }
+    AJAXObj.open("GET", "/cancel?name=" + inputName.value + "&day=" + inputDay.value + "&time=" + inputTime.value);
+    AJAXObj.send();
+}
+
+function checkAvailability()
+{
+    let AJAXObj = new XMLHttpRequest();
+    AJAXObj.onload = function()
+    {
+        if (this.status == 200)
+        {
+            output.innerHTML = this.responseText;   
+        }
+        else
+        {
+            errorHandler("Status: " + this.status + this.responseText);
+        }
+    };
+    AJAXObj.onerror = function()
+    {
+        errorHandler("Connection error. Try again later.");
+    }
+    AJAXObj.open("GET", "/check?day=" + inputDay.value + "&time=" + inputTime.value);
+    AJAXObj.send();
+}
